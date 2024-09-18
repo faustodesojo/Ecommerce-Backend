0b5a2e2c2dcd3256a54c18b0bdd1ff40b0031dce
Fausto Desojo <faustodesojo@hotmail.com>
2024-09-18 11:52:50 -0300

    footer modificaton

--- ecommerce-frontend/src/components/footer/footer.tsx
+++ ecommerce-frontend/src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./footer.css";
 import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
@@ -9,6 +10,24 @@ const Footer: React.FC = () => {
         <img src="images/logo.png" alt="Logo" />
       </div>
       <ul className="footer-container">
+        <div className="redes">
+            <h3>Redes</h3>
+          <li>
+            <FaWhatsapp /> +549 221123456
+          </li>
+          <li>
+            <FaInstagram /> @alambardosbelgrano
+          </li>
+          <li>
+            <FaFacebook /> @alambardosbelgrano
+          </li>
+        </div>
+        <div className="contacto">
+            <h3>Contacto</h3>
+          <Link to="/contacto">Contáctanos</Link>
+          <Link to= "/cart">Mi pedido</Link>
+
+    </div>
         <div className="redes">
             <h3>Redes</h3>
           <li>
