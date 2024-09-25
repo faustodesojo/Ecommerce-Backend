b64bd6f5581cdfcb750b4cda32906dce77a35ab1
Fausto <faustodesojo@hotmail.com>
2024-09-25 18:51:34 -0300

    add cart and login to navbar

--- ecommerce-frontend/src/components/navbar/navbar.tsx
+++ ecommerce-frontend/src/components/navbar/navbar.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import { CiShoppingCart } from "react-icons/ci";
+import { VscAccount } from "react-icons/vsc";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,13 +17,41 @@ const Navbar: React.FC = () => {
         <img src="images/logo.png" alt="Logo" className="navbar-logo-img" />
         <Link to="/">Alambrados Belgrano</Link>
       </div>
-      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
-        <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
-        <li><Link to="/products" onClick={() => setIsOpen(false)}>Productos</Link></li>
-        <li><Link to="/contacto" onClick={() => setIsOpen(false)}>Contacto</Link></li>
-        <li><Link to="/cart" onClick={() => setIsOpen(false)}>Cart</Link></li>
+      <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <Link to="/" onClick={() => setIsOpen(false)}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/products" onClick={() => setIsOpen(false)}>
+            Productos
+          </Link>
+        </li>
+        <li>
+          <Link to="/contacto" onClick={() => setIsOpen(false)}>
+            Contacto
+          </Link>
+        </li>
+        <li>
+          <div className="cart-login">
+            <div className="cart">
+              <Link to="/cart" onClick={() => setIsOpen(false)}>
+                <CiShoppingCart />
+              </Link>
+            </div>
+            <div className="login">
+              <Link to="/login" onClick={() => setIsOpen(false)}>
+                <VscAccount />
+              </Link>
+            </div>
+          </div>
+        </li>
       </ul>
-      <div className={`navbar-toggle ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+      <div
+        className={`navbar-toggle ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
         <div className="navbar-toggle-icon"></div>
         <div className="navbar-toggle-icon"></div>
         <div className="navbar-toggle-icon"></div>
